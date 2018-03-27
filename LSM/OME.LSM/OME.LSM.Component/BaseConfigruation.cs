d914657845d666b5e3d9e0e5d9a28dc8985ab9e3
A1 <A1@TY-04-IT-040151>
2018-03-27 18:35:48 +0800

    提交更改

---
+++ LSM/OME.LSM/OME.LSM.Component/BaseConfigruation.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OME.Unity;
+using Orleans;
+namespace OME.LSM.Component
+{
+    public interface IBaseConfigruation : IDisposable
+    {
+        Task Configurations();
+    }
+
+    public class BaseConfigruation : Grain, IBaseConfigruation
+    {
+        public async Task Configurations()
+        {
+            await Task.Run(() =>
+            {
+                UnityIoc.containerName = "MyContainer";
+                UnityIoc.filepath = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "") + "Unity.config";
+            });
+
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ LSM/OME.LSM/OME.LSM.Facade/BaseConfigruation.cs
@@ -0,0 +1,36 @@
+﻿using OME.Unity;
+using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OME.LSM.Facade
+{
+   public interface  IBaseConfigruation:IDisposable
+    {
+        Task Configurations();
+    }
+
+
+
+
+    public class BaseConfigruation : Grain, IBaseConfigruation
+    {
+        public async Task Configurations()
+        {
+            await Task.Run(() =>
+            {
+                UnityIoc.containerName = "MyContainer";
+                UnityIoc.filepath = AppDomain.CurrentDomain.BaseDirectory.Replace("bin\\Debug\\", "") + "Unity.config";
+            });
+           
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
